9f0e01bb3c450e527548840aea9c4f10e6281fa6
jerowe <jillian.e.rowe@gmail.com>
2016-09-23 17:48:07 +0400

    adding dyld path

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,16 @@ export CFLAGS="-I${PREFIX}/include ${CFLAGS}"
 export LDFLAGS="-L${PREFIX}/lib ${LDFLAGS}"
 export CPPFLAGS="-I${PREFIX}/include ${CPPFLAGS}"
 
+export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
+export DYLD_LIBRARY_PATH="${PREFIX}/lib"
+
 if [ `uname -m` == ppc64le ]; then
     B="--build=ppc64le-linux"
 fi
 
 ./configure $B --enable-threadsafe \
             --enable-tempstore \
+            --enable-readline \
             --enable-shared=yes \
             --disable-tcl \
             --prefix=$PREFIX
