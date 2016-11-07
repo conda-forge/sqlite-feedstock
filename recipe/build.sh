a4b14b1c18953c2126a2a07f085ecf7c0ed6d52e
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-11-07 13:45:00 +0400

    adding back in changes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
+export PATH=${PREFIX}/bin:$PATH
+export CFLAGS="-I${PREFIX}/include"
+
+export LDFLAGS="-L${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include"
+
+export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
+export DYLD_LIBRARY_PATH="${PREFIX}/lib"
+
 if [ `uname -m` == ppc64le ]; then
     B="--build=ppc64le-linux"
 fi
 
-#--disable-readline \
-
 ./configure $B --enable-threadsafe \
             --enable-tempstore \
+            --enable-readline \
             --enable-shared=yes \
             --disable-tcl \
             --prefix=$PREFIX
+
+
+cp ${RECIPE_DIR}/Makefile.patch ./Makefile.patch
+
+patch < Makefile.patch
+
 make
 make check
 make install
