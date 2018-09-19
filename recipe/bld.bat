34cd8c4d1ca9d54208242e49953461b58280ce25
Filipe Fernandes <ocefpaf@gmail.com>
2018-09-19 10:47:08 -0300

    add SQLITE_ENABLE_GEOPOLY support

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,10 +5,10 @@ set PLATFORM=x64
 )
 
 :: build the shell
-cl /DSQLITE_ENABLE_RTREE shell.c sqlite3.c -Fesqlite3.exe /DSQLITE_EXPORTS
+cl /DSQLITE_ENABLE_RTREE /DSQLITE_ENABLE_GEOPOLY shell.c sqlite3.c -Fesqlite3.exe /DSQLITE_EXPORTS
 
 :: build the dll
-cl /DSQLITE_ENABLE_RTREE sqlite3.c -link -dll -out:sqlite3.dll
+cl /DSQLITE_ENABLE_RTREE /DSQLITE_ENABLE_GEOPOLY sqlite3.c -link -dll -out:sqlite3.dll
 
 COPY sqlite3.exe %LIBRARY_BIN% || exit 1
 COPY sqlite3.dll %LIBRARY_BIN% || exit 1
