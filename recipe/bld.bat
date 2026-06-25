f3e79df2927a1b2bc8ce5215635f2cbbdae835f7
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2026-06-25 16:27:50 +0100

    Add missing sqlite3.pc on Windows (Closes #165)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -42,3 +42,4 @@ COPY sqlite3.dll  %LIBRARY_BIN% || exit 1
 COPY sqlite3.lib  %LIBRARY_LIB% || exit 1
 COPY sqlite3.h    %LIBRARY_INC% || exit 1
 COPY sqlite3ext.h %LIBRARY_INC% || exit 1
+COPY sqlite3.pc   %LIBRARY_LIB%\pkgconfig\ || exit 1
